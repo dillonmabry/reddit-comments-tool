607426802ff908d1a94d75b6eba3a6ec27eec7b2
Dillon Mabry <dcmabry@gmail.com>
2019-05-03 21:01:10 -0400

    README for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup
 
-setup(name = 'reddit_extract',
-    version = '0.1.2',
-    description = 'Extract reddit comments and general API helper',
-    author = 'Dillon Mabry',
-    author_email = 'rapid.dev.solutions@gmail.com',
-    license = 'MIT',
-    packages = ['reddit_extract'],
-    test_suite = 'nose.collector',
-    tests_require = ['nose'],
-    install_requires = ['praw', 'requests'],
-    include_package_data = True,
-    data_files = [('', [
-        'reddit_extract/resources/search_template.txt'
-    ])],
-    zip_safe = False)
+
+def readme():
+      with open('README.md') as f:
+            return f.read()
+
+
+setup(name='reddit_extract',
+      version='0.1.3',
+      description=readme(),
+      author='Dillon Mabry',
+      author_email='rapid.dev.solutions@gmail.com',
+      license='MIT',
+      packages=['reddit_extract'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=['praw', 'requests'],
+      include_package_data=True,
+      data_files=[('', [
+          'reddit_extract/resources/search_template.txt'
+      ])],
+      zip_safe=False)
